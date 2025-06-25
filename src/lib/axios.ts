19db5c598e14bc778d6a186e13204e35c6c4cb9f
Samuel <samuel@thegreg.ai>
2025-06-25 18:11:07 +0000

    services

---
+++ src/lib/axios.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const authInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  timeout: 50000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
+});
+
+export { authInstance };
---
+++ src/services/auth.service.ts
@@ -0,0 +1,96 @@
+import { authInstance } from "@/lib/axios";
+import { AxiosError } from "axios";
+import { SigninType, SignupType } from "@/types";
+
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL
+
+export const signinHandler = async (userCredentials: SigninType) => {
+    try {
+      const response = await authInstance.post("/api/v1/auth/login", userCredentials);
+      if (response.status === 200) {
+        localStorage.setItem("access_token", response.data.data.token)
+        return {
+          success: true,
+          message: "Login successful",
+          data: response.data,
+        };
+      }
+  
+      return {
+        success: false,
+        message: "Unexpected response from server",
+      };
+    } catch (error) {
+      const err = error as AxiosError<any>;
+      if (err.response) {
+        const { status, data } = err.response;
+  
+        if (status === 404 && data.message) {
+          return {
+            success: false,
+            message: data.message || "No account found",
+          };
+        }
+  
+        return {
+          success: false,
+          message: data?.message || "An error occurred during sign in.",
+        };
+      }
+  
+      console.error("Sign-in error:", error);
+      return {
+        success: false,
+        message: "Network error. Please try again.",
+      };
+    }
+  };
+
+  export const signupHandler = async (userCredentials: SignupType) => {
+    try {
+      const response = await authInstance.post("/api/v1/auth/register", userCredentials);
+  
+      if (response.status === 200 || response.status === 201) {
+        console.log(response.data)
+        return {
+          success: true,
+          message: response.data?.message || "Signup successful",
+          data: response.data,
+        };
+      }
+  
+      return {
+        success: false,
+        message: "Unexpected server response.",
+      };
+    } catch (error) {
+      const err = error as AxiosError<any>;
+  
+      if (err.response) {
+        const { data, status } = err.response;
+  
+        return {
+          success: false,
+          message: data?.message || `Signup failed with status ${status}.`,
+        };
+      }
+  
+      console.error("Signup error:", error);
+      return {
+        success: false,
+        message: "Network error. Please try again.",
+      };
+    }
+  };
+
+  export const oauthHandler = async (provider: string) => {
+    try {
+      const response = await fetch(`${baseURL}/v1/auth/login/${provider}`);
+      const data = await response.json()
+      console.log(data)
+      window.location.href = data.data.redirect_url
+    } catch (error) {
+      console.error("error during oauth", error)
+    }
+  
+  };
