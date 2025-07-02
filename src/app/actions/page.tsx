b3622938f518a5d90331a1e0bdcfbe1c68fcc47b
Samuel <samuel@thegreg.ai>
2025-07-02 09:31:50 +0000

    fix layout

--- src/app/actions/page.tsx
+++ src/app/actions/page.tsx
@@ -8,12 +8,14 @@ function Page() {
   const profilePicture = user?.profilePicture;
   console.log("userrrrr", user);
   return (
-    <div className="h-screen">
+    <div className="h-screen w-screen flex flex-col">
       <div>
         <p>{`Hello and welcome ${firstName}`}</p>
       </div>
-      <div className="flex justify-end p-2 items-end ">
-        <Chatbox />
+      <div className="flex-1 flex items-end justify-end p-4">
+        <div className="bg-primary/80 border-2 border-primary rounded-xl shadow-lg p-3 animate-in fade-in flex items-end justify-end">
+          <Chatbox />
+        </div>
       </div>
     </div>
   );
