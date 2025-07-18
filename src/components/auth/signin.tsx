9f7bcbddee5166759f0c1dfc991974b0c58df4ee
Samuel <samuel@thegreg.ai>
2025-07-18 08:03:17 +0000

    login with popup

--- src/components/auth/signin.tsx
+++ src/components/auth/signin.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import { Button } from "../ui/button";
 import { useAuth0 } from "@auth0/auth0-react";
 function SignInButton() {
-  const { loginWithRedirect } = useAuth0();
-  return <Button onClick={() => loginWithRedirect()}>Signin</Button>;
+  const { loginWithPopup } = useAuth0();
+  return <Button onClick={() => loginWithPopup()}>Signin</Button>;
 }
 
 export default SignInButton;
